7349f05267d2064498543d494716cfcdc37047c0
Ralph Slooten <axllent@gmail.com>
2013-02-13 10:44:58 +1300

    Add other filetypes

--- javascript/linkstoblank.js
+++ javascript/linkstoblank.js
@@ -2,7 +2,7 @@ var a = document.getElementsByTagName("a");
 for(i = 0; i < a.length; i++){
 	if(
 		( a[i].href.indexOf(location.host) == -1 && a[i].href.match(/^https?\:\/\//i) )
-		|| a[i].href.match(/\.(pdf|docx?|xlsx?)$/i)
+		|| a[i].href.match(/\.(pdf|docx?|xlsx?|zip|gz|bz2|rar)$/i)
 	)
 		a[i].target = "_blank";
 }
