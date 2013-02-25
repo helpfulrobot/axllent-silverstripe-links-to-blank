96cc5c945d0b7e7b36c027fe35787cdbe32cbfe9
Ralph Slooten <axllent@gmail.com>
2013-02-25 14:57:27 -0800

    target="_blank" on domload

--- javascript/linkstoblank.js
+++ javascript/linkstoblank.js
@@ -1,12 +1,17 @@
-function _l2b(event){
-	var el = event.srcElement || event.target;
-	if(el.tagName.toLowerCase() == 'a' && el.href
-		&& (el.href.indexOf(location.host) == -1 && el.href.match(/^https?\:\/\//i)
-		|| el.href.match(/\.(pdf|docx?|pp(s|tx?)|xlsx?|zip|gz|bz2|(r|t)ar|7z)$/i))
-	)
-		el.target = "_blank";
+function _l2b(){
+	var a = document.getElementsByTagName("a");
+	for(i = 0; i<a.length; i++){
+		if(
+			(a[i].href.indexOf(location.host) == -1 && a[i].href.match(/^https?\:\/\//i))
+			|| a[i].href.match(/\.(pdf|docx?|pp(s|tx?)|xlsx?|zip|gz|bz2|(r|t)ar|7z)$/i)
+		)
+			a[i].target = "_blank";
+	}
 }
 
-var d = document;
-if(d.addEventListener)d.addEventListener("click",_l2b);
-else if(d.attachEvent)d.attachEvent("onclick",_l2b);
+var w = window;
+
+if(w.addEventListener)
+	w.addEventListener("load",_l2b);
+else if (w.attachEvent)
+	w.attachEvent("onload",_l2b);
